45232ecc41a0103896c2791a881eb59316fe7797
Artsiom Kraskouski <artem.kraskovski@gmail.com>
2021-01-23 00:08:14 +0300

    Initial commit

---
+++ src/test/java/com/github/akraskovski/app/SpringBootGraalvmNativeImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.akraskovski.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraalvmNativeImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
